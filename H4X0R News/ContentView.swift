75f906363e17284a34393c94247a8fb33022e434
torinunna <torinunna9588@gmail.com>
2022-05-15 22:39:20 +0900

    Understanding the List and Identifiable Protocol

--- H4X0R News/ContentView.swift
+++ H4X0R News/ContentView.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+        NavigationView{
+            List(posts) { post in
+                Text(post.title)
+            }
+            .navigationTitle("H4X0R NEWS")
+            }
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -19,3 +23,14 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct Post: Identifiable {
+    let id: String
+    let title: String
+}
+
+let posts = [
+    Post(id: "1", title: "Hello"),
+    Post(id: "2", title: "Bonjour"),
+    Post(id: "3", title: "Hola"),
+]
